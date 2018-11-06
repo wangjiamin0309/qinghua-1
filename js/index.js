534bc18eb545f545a9810c11fe4e75d7acffeffa
wangjiamin0309 <377858464@qq.com>
2018-11-06 09:59:44 +0800

    modify

--- js/index.js
+++ js/index.js
@@ -19,26 +19,41 @@ $('li.dropdown').mouseover(function() {
 //				$(this).parent().removeClass('open')
 //			})
 //		})
-	$('.ellipsis-tow').each(function(){
-	 //设置显示获取字符串的字数  这个根绝要求 看需要大概显示几行
-	    var maxwidth=26;   
-	    if($(this).text().length>maxwidth){
-	         //截取字符串
-	        $(this).text($(this).text().substring(0,maxwidth));
-	        //多余的用省略号显示
-	        $(this).html($(this).html()+'...');
-	    }
-	});
-	$('.ellipsis-three').each(function(){
-	 //设置显示获取字符串的字数  这个根绝要求 看需要大概显示几行
-	    var maxwidth=160;   
-	    if($(this).text().length>maxwidth){
-	         //截取字符串
-	        $(this).text($(this).text().substring(0,maxwidth));
-	        //多余的用省略号显示
-	        $(this).html($(this).html()+'...');
-	    }
-	});
+$('.ellipsis-tow').each(function(){
+ //设置显示获取字符串的字数  这个根绝要求 看需要大概显示几行
+    var maxwidth=26;   
+    if($(this).text().length>maxwidth){
+         //截取字符串
+        $(this).text($(this).text().substring(0,maxwidth));
+        //多余的用省略号显示
+        $(this).html($(this).html()+'...');
+    }
+});
+$('.ellipsis-three').each(function(){
+ //设置显示获取字符串的字数  这个根绝要求 看需要大概显示几行
+    var maxwidth=160;   
+    if($(this).text().length>maxwidth){
+         //截取字符串
+        $(this).text($(this).text().substring(0,maxwidth));
+        //多余的用省略号显示
+        $(this).html($(this).html()+'...');
+    }
+});
+//导航跳转对应的选线卡内容
+var hash = location.hash;	
+console.log(hash)
+var tab = $('.click-nav li');
+var con = $('.container>.tab-content>.tab-pane');	
+for(var i=0;i<con.length;i++){
+	var mm = con[i];
+	var selectCon = "#"+ $(mm).attr('id');
+	if(hash == selectCon){
+		tab.siblings().removeClass('active');
+		con.siblings().removeClass('active');
+		$(tab[i]).addClass('active');
+		$(con[i]).addClass('active in');
+	}
+}
 //$('.ellipsis-small-tow').each(function(){
 // //设置显示获取字符串的字数  这个根绝要求 看需要大概显示几行
 //  var maxwidth=50;   
